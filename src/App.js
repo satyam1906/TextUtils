3bd40db74eabd4e0107be80915f774240095f63e
Satyam <satyam9322@gmail.com>
2022-03-18 11:26:04 +0530

    Removed Routers

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import { useState } from "react";
 import "./App.css";
 import Alert from "./components/Alert";
-import About from "./components/About";
+// import About from "./components/About";
 import Navbar from "./components/Navbar";
 import TextForm from "./components/TextForm";
-import { BrowserRouter , Routes, Route } from "react-router-dom"; /*Import BrowserRouter(do not rename it as 'Router'), Routes(in place of 'Switch') and Route from react-router-dom */
+// import { BrowserRouter , Routes, Route } from "react-router-dom"; /*Import BrowserRouter(do not rename it as 'Router'), Routes(in place of 'Switch') and Route from react-router-dom */
 // props->Properties
 function App() {
   const [mode, setMode] = useState("light"); //Whether dark mode is enabled or not
@@ -34,29 +34,29 @@ function App() {
     <>
       {/* <Navbar title="TextUtils" aboutText="About TextUtils" /> */}
       {/* <Navbar/> */}
-      <BrowserRouter> {/*According to CWH, This is supposed to be Router because BrowserRouter was renamed to 'Router' while importing */}
+      {/* <BrowserRouter> According to CWH, This is supposed to be Router because BrowserRouter was renamed to 'Router' while importing */}
         <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} aboutText="About" />
         <Alert alert={alert} />
         <div className="container my-3">
-          <Routes>
+          {/* <Routes> */}
              {/** If you dont use 'exact',React may produce an error in rendering the following path as React generally does Partial Matching unless Specified 
              /users --> Component 1
         /users/home --> Component 2 */}
-            <Route exact path="/about" element={<About mode={mode} />}></Route>
+            {/* <Ro ute exact path="/about" element={<About mode={mode} />}></Ro> */}
 
-            <Route
-              exact path="/"
-              element={
+            {/* <Route */}
+              {/* exact path="/"
+              element={ */}
                 <TextForm
                   showAlert={showAlert}
                   heading="Enter the text to analyze below"
                   mode={mode}
                 />
-              }
-            ></Route>
-          </Routes>
+              {/* } */}
+            {/* ></Route> */}
+          {/* </Routes> */}
         </div>
-      </BrowserRouter>
+      {/* </BrowserRouter> */}
 
       {/* <About /> */}
     </>
